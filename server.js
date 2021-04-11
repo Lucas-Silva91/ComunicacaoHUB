f53912bab58e8acef8f66c048bbb6127fc458567
Lucas Silva <lcristiano.dev@gmail.com>
2021-04-11 14:21:59 -0300

    Projeto Inicializado

---
+++ server.js
@@ -0,0 +1,8 @@
+
+const http = require ('http');
+const port = process.env.PORT || 3000;
+const app = require ('./app')
+
+const server = http.createServer(app);
+
+server.listen(port, (req, res)=>console.log(`Server running on http://localhost:${port}`))
